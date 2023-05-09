d396191e52e53df460ed4e43e6cfcab496d3b0c4
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-05-10 00:08:17 +0530

    Update subsequenceOfString.java

--- subsequenceOfString.java
+++ subsequenceOfString.java
@@ -1,1 +1,31 @@
 
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+class Solution {
+    public static ArrayList<String> subsequences(String str) {
+        ArrayList<String> ans=new ArrayList<String>();
+        String output = new String("");
+        int index = 0;
+        solve(str, output, index, ans);
+        return ans;
+    }
+    
+    public static void solve(String str, String output, int index, ArrayList<String> ans) {
+        if(index >= str.length()) {
+            if(output.length() > 0) {
+                ans.add(new String(output));
+            }
+            return;
+        }
+
+        // include case
+        output += str.substring(index, index+1);
+        solve(str, output, index+1, ans);
+
+        // exclude case
+        output = output.substring(0, output.length()-1);
+        solve(str, output, index+1, ans);
+    }
+}
