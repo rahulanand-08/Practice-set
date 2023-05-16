fb4793e0525b3b5a15431cb747096b02f00ec230
Rahul Anand Sahoo <101013741+rahulanand-08@users.noreply.github.com>
2023-05-16 23:24:28 +0530

    Update MiddleOfLinkedList.java

--- MiddleOfLinkedList.java
+++ MiddleOfLinkedList.java
@@ -1,1 +1,17 @@
+//Link:-https://www.codingninjas.com/codestudio/problems/middle-of-linked-list_973250?source=youtube&campaign=Lovebabbarcodestudio_24thJan&leftPanelTab=1
 
+
+public class Solution {
+    public static Node findMiddle(Node head) 
+    {
+        // Write your code here
+        Node slow = head;
+        Node fast=head;
+        while(fast != null && fast.next != null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
